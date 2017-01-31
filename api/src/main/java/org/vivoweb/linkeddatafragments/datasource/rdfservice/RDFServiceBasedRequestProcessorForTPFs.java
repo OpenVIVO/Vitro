1496e06362565ede6d097d5439f571df18888430
Graham Triggs <gtriggs@duraspace.org>
2017-01-31 19:28:01 +0000

    REVERSE BEFORE UPGRADE - Fix blank nodes and add client interface

--- api/src/main/java/org/vivoweb/linkeddatafragments/datasource/rdfservice/RDFServiceBasedRequestProcessorForTPFs.java
+++ api/src/main/java/org/vivoweb/linkeddatafragments/datasource/rdfservice/RDFServiceBasedRequestProcessorForTPFs.java
@@ -1,5 +1,11 @@
 package org.vivoweb.linkeddatafragments.datasource.rdfservice;
 
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.graph.NodeFactory;
+import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.rdf.model.AnonId;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 import edu.cornell.mannlib.vitro.webapp.controller.VitroRequest;
 import edu.cornell.mannlib.vitro.webapp.dao.jena.QueryUtils;
 import edu.cornell.mannlib.vitro.webapp.rdfservice.RDFService;
@@ -74,6 +80,32 @@ public class RDFServiceBasedRequestProcessorForTPFs
             return sw.toString();
         }
 
+        private Node skolemize(Node node) {
+            if (node != null && node.isBlank()) {
+                return NodeFactory.createURI("bnode://" + node.getBlankNodeLabel());
+            }
+
+            return node;
+        }
+
+        private RDFNode deskolemize(RDFNode node) {
+            if (node == null) {
+                return null;
+            }
+
+            if (node.isResource()) {
+                String uri = node.asResource().getURI();
+                if (uri != null && uri.startsWith("bnode://")) {
+                    String bnodeId = uri.substring(8);
+                    return ModelFactory.createDefaultModel().asRDFNode(
+                            NodeFactory.createAnon(new AnonId(bnodeId))
+                    );
+                }
+            }
+
+            return node;
+        }
+
         @Override
         protected ILinkedDataFragment createFragment(
                    final ITriplePatternElement<RDFNode,String,String> subject,
@@ -83,15 +115,34 @@ public class RDFServiceBasedRequestProcessorForTPFs
                    final long limit )
         {
             try {
-                RDFNode nSubject = subject.isVariable() ? null : subject.asConstantTerm();
-                RDFNode nPredicate = predicate.isVariable() ? null : predicate.asConstantTerm();
-                RDFNode nObject = object.isVariable() ? null : object.asConstantTerm();
+                RDFNode nSubject = subject.isVariable() ? null : deskolemize(subject.asConstantTerm());
+                RDFNode nPredicate = predicate.isVariable() ? null : deskolemize(predicate.asConstantTerm());
+                RDFNode nObject = object.isVariable() ? null : deskolemize(object.asConstantTerm());
 
                 Model triples = rdfService.getTriples(nSubject, nPredicate, nObject, limit, offset);
                 if (triples == null || triples.isEmpty()) {
                     return createEmptyTriplePatternFragment();
                 }
 
+                if (triples.size() > 0) {
+                    Model replacedBlankNodes = ModelFactory.createDefaultModel();
+                    StmtIterator iter = triples.listStatements();
+                    while (iter.hasNext()) {
+                        Statement oldStmt = iter.next();
+                        Triple t = oldStmt.asTriple();
+                        replacedBlankNodes.add(
+                                replacedBlankNodes.asStatement(
+                                        new Triple(
+                                                skolemize(t.getSubject()),
+                                                skolemize(t.getPredicate()),
+                                                skolemize(t.getObject())
+                                        )
+                                )
+                        );
+                    }
+
+                    triples = replacedBlankNodes;
+                }
                 long size = triples.size();
                 long estimate = -1;
                 estimate = rdfService.countTriples(nSubject, nPredicate, nObject);
